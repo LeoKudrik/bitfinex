27ac35410e469151984f9dfcc25c858e4ffa65af
Leo Kudrik <kl@findnewclient.com>
2017-08-05 23:48:56 +0300

    ref

--- FinexAPI/FinexAPI.py
+++ FinexAPI/FinexAPI.py
@@ -1,4 +1,5 @@
-# Python implementation. Written by jndok - 2014. Distributed under GNU/GPL v3 (http://opensource.org/licenses/GPL-3.0). [v. 0.0.1-4]
+# Python implementation. Written by jndok - 2014. Distributed under GNU/GPL v3
+# (http://opensource.org/licenses/GPL-3.0). [v. 0.0.1-4]
 # Edited by Dawson Botsford
 # report ANY bug @ https://github.com/dawsonbotsford/bitfinex/issues
 
@@ -9,373 +10,401 @@ import hmac
 import hashlib
 import time
 
-__all__ = ['ticker', 'today', 'orderbook', 'lendbook', 'stats', 'trades', 'lends', 'symbols', 'place_order', 'delete_order', 'delete_all_order', 'status_order', 'active_orders', 'active_positions', 'place_offer', 'cancel_offer', 'status_offer', 'active_offers', 'past_trades', 'balances', 'claim_position', 'close_position', 'withdraw']
+__all__ = ['ticker', 'today', 'orderbook', 'lendbook', 'stats', 'trades',
+           'lends', 'symbols', 'place_order', 'delete_order',
+           'delete_all_order', 'status_order', 'active_orders',
+           'active_positions', 'place_offer', 'cancel_offer', 'status_offer',
+           'active_offers', 'past_trades', 'balances', 'claim_position',
+           'close_position', 'withdraw']
 
 URL = "https://api.bitfinex.com/v1"
 
-fp = open("../keys.txt")
-
-API_KEY = fp.readline().rstrip() # put your API public key here.
-API_SECRET = fp.readline().rstrip() # put your API private key here.
-print ("Your pub: " + str(API_KEY))
-#print "Your priv: " + str(API_SECRET)
-
-# unauthenticated
-
-def ticker(symbol='btcusd'): # gets the innermost bid and asks and information on the most recent trade.
-
-	r = requests.get(URL + "/pubticker/" + symbol, verify=True) # <== UPDATED TO LATEST VERSION OF BFX!
-	rep = r.json()
-
-	try:
-		rep['last_price']
-	except KeyError:
-		return rep['message']
-
-	return rep
-
-def stats(symbol='btcusd'): # Various statistics about the requested pairs.
-
-	r = requests.get(URL + "/stats/" + symbol, verify=True) # <== UPDATED TO LATEST VERSION OF BFX!
-	rep = r.json()
-	return rep
-
-	try:
-		rep['volume']
-	except KeyError:
-		return rep['message']
+API_KEY = ''
+API_SECRET = ''
 
 
-def today(symbol='btcusd'): # today's low, high and volume.
+def set_auth_data(api_key, api_secret):
+    global API_KEY, API_SECRET
+    API_KEY, API_SECRET = api_key, api_secret
 
-	r = requests.get(URL + "/today/" + symbol, verify=True)
-	rep = r.json()
 
-	try:
-		rep['volume']
-	except KeyError:
-		return rep['message']
+def ticker(symbol='btcusd'):
+    # gets the innermost bid and asks and information on the most recent trade.
 
-	return rep
+    r = requests.get(URL + "/pubticker/" + symbol,
+                     verify=True)  # <== UPDATED TO LATEST VERSION OF BFX!
+    rep = r.json()
 
-def orderbook(symbol='btcusd'): # get the full order book.
+    try:
+        rep['last_price']
+    except KeyError:
+        return rep['message']
 
-	r = requests.get(URL + "/book/" + symbol, verify=True)
-	rep = r.json()
+    return rep
 
-	return rep
 
-def lendbook(currency='btc'): # get the full lend book.
+def stats(symbol='btcusd'):  # Various statistics about the requested pairs.
 
-	r = requests.get(URL + "/lendbook/" + currency, verify=True)
-	rep = r.json()
+    r = requests.get(URL + "/stats/" + symbol,
+                     verify=True)  # <== UPDATED TO LATEST VERSION OF BFX!
+    rep = r.json()
+    return rep
 
-	return rep
 
-def trades(symbol='btcusd'): # get a list of the most recent trades for the given symbol.
+def today(symbol='btcusd'):  # today's low, high and volume.
 
-	r = requests.get(URL + "/trades/" + symbol, verify=True)
-	rep = r.json()
+    r = requests.get(URL + "/today/" + symbol, verify=True)
+    rep = r.json()
 
-	return rep
+    try:
+        rep['volume']
+    except KeyError:
+        return rep['message']
 
-def lends(currency='btc'): # get a list of the most recent lending data for the given currency: total amount lent and rate (in % by 365 days).
+    return rep
 
-	r = requests.get(URL + "/lends/" + currency, verify=True)
-	rep = r.json()
 
-	return rep
+def orderbook(symbol='btcusd'):  # get the full order book.
 
-def symbols(): # get a list of valid symbol IDs.
+    r = requests.get(URL + "/book/" + symbol, verify=True)
+    rep = r.json()
 
-	r = requests.get(URL + "/symbols", verify=True)
-	rep = r.json()
+    return rep
 
-	return rep
+
+def lendbook(currency='btc'):  # get the full lend book.
+
+    r = requests.get(URL + "/lendbook/" + currency, verify=True)
+    rep = r.json()
+
+    return rep
+
+
+def trades(symbol='btcusd'):
+    # get a list of the most recent trades for the given symbol.
+
+    r = requests.get(URL + "/trades/" + symbol, verify=True)
+    rep = r.json()
+
+    return rep
+
+
+def lends(currency='btc'):
+    # get a list of the most recent lending data for the given currency:
+    # total amount lent and rate (in % by 365 days).
+
+    r = requests.get(URL + "/lends/" + currency, verify=True)
+    rep = r.json()
+
+    return rep
+
+
+def symbols():  # get a list of valid symbol IDs.
+
+    r = requests.get(URL + "/symbols", verify=True)
+    rep = r.json()
+
+    return rep
+
 
 # authenticated
 
-def genNonce(): # generates a nonce, used for authentication.
-	return str(long(time.time() * 1000000))
+def gen_nonce():  # generates a nonce, used for authentication.
+    return str(int(time.time() * 1000000))
 
-def payloadPacker(payload): # packs and signs the payload of the request.
 
-	j = json.dumps(payload)
-	data = base64.standard_b64encode(j)
+def payload_packer(payload):  # packs and signs the payload of the request.
 
-	h = hmac.new(API_SECRET, data, hashlib.sha384)
-	signature = h.hexdigest()
+    j = json.dumps(payload)
+    data = base64.standard_b64encode(j)
 
-	return {
-		"X-BFX-APIKEY": API_KEY,
-		"X-BFX-SIGNATURE": signature,
-		"X-BFX-PAYLOAD": data
-	}
+    h = hmac.new(API_SECRET, data, hashlib.sha384)
+    signature = h.hexdigest()
 
-def place_order(amount, price, side, ord_type, symbol='btcusd', exchange='bitfinex'): # submit a new order.
+    return {
+        "X-BFX-APIKEY": API_KEY,
+        "X-BFX-SIGNATURE": signature,
+        "X-BFX-PAYLOAD": data
+    }
 
-	payload = {
 
-		"request":"/v1/order/new",
-		"nonce":genNonce(),
-		"symbol":symbol,
-		"amount":amount,
-		"price":price,
-		"exchange":exchange,
-		"side":side,
-		"type":ord_type
+def place_order(amount, price, side, ord_type, symbol='btcusd',
+                exchange='bitfinex'):  # submit a new order.
 
-	}
+    payload = {
 
-	signed_payload = payloadPacker(payload)
-	r = requests.post(URL + "/order/new", headers=signed_payload, verify=True)
-	rep = r.json()
+        "request": "/v1/order/new",
+        "nonce": gen_nonce(),
+        "symbol": symbol,
+        "amount": amount,
+        "price": price,
+        "exchange": exchange,
+        "side": side,
+        "type": ord_type
 
-	try:
-		rep['order_id']
-	except:
-		return rep['message']
+    }
 
-	return rep
+    signed_payload = payload_packer(payload)
+    r = requests.post(URL + "/order/new", headers=signed_payload, verify=True)
+    rep = r.json()
 
-def delete_order(order_id): # cancel an order.
+    try:
+        rep['order_id']
+    except:
+        return rep['message']
 
-	payload = {
+    return rep
 
-		"request":"/v1/order/cancel",
-		"nonce":genNonce(),
-		"order_id":order_id
 
-	}
+def delete_order(order_id):  # cancel an order.
 
-	signed_payload = payloadPacker(payload)
-	r = requests.post(URL + "/order/cancel", headers=signed_payload, verify=True)
-	rep = r.json()
+    payload = {
 
-	try:
-		rep['avg_execution_price']
-	except:
-		return rep['message']
+        "request": "/v1/order/cancel",
+        "nonce": gen_nonce(),
+        "order_id": order_id
 
-	return rep
+    }
 
-def delete_all_order(): # cancel an order.
+    signed_payload = payload_packer(payload)
+    r = requests.post(URL + "/order/cancel", headers=signed_payload,
+                      verify=True)
+    rep = r.json()
 
-	payload = {
+    try:
+        rep['avg_execution_price']
+    except:
+        return rep['message']
 
-		"request":"/v1/order/cancel/all",
-		"nonce":genNonce(),
+    return rep
 
-	}
 
-	signed_payload = payloadPacker(payload)
-	r = requests.post(URL + "/order/cancel/all", headers=signed_payload, verify=True)
-	rep = r.json()
-	return rep
-'''
-	try:
-		rep['avg_execution_price']
-	except:
-		return rep['message']
-'''
+def delete_all_order():  # cancel an order.
 
-def status_order(order_id): # get the status of an order. Is it active? Was it cancelled? To what extent has it been executed? etc.
+    payload = {
 
-	payload = {
+        "request": "/v1/order/cancel/all",
+        "nonce": gen_nonce(),
 
-		"request":"/v1/order/status",
-		"nonce":genNonce(),
-		"order_id":order_id
+    }
 
-	}
+    signed_payload = payload_packer(payload)
+    r = requests.post(URL + "/order/cancel/all", headers=signed_payload,
+                      verify=True)
+    rep = r.json()
+    return rep
 
-	signed_payload = payloadPacker(payload)
-	r = requests.post(URL + "/order/status", headers=signed_payload, verify=True)
-	rep = r.json()
 
-	try:
-		rep['avg_execution_price']
-	except:
-		return rep['message']
+def status_order(order_id):
+    # get the status of an order. Is it active? Was it cancelled?
+    # To what extent has it been executed? etc.
 
-	return rep
+    payload = {
 
-def active_orders(): # view your active orders.
+        "request": "/v1/order/status",
+        "nonce": gen_nonce(),
+        "order_id": order_id
 
-	payload = {
+    }
 
-		"request":"/v1/orders",
-		"nonce":genNonce()
+    signed_payload = payload_packer(payload)
+    r = requests.post(URL + "/order/status", headers=signed_payload,
+                      verify=True)
+    rep = r.json()
 
-	}
+    try:
+        rep['avg_execution_price']
+    except:
+        return rep['message']
 
-	signed_payload = payloadPacker(payload)
-	r = requests.post(URL + "/orders", headers=signed_payload, verify=True)
-	rep = r.json()
+    return rep
 
-	return rep
 
-def active_positions(): # view your active positions.
+def active_orders():  # view your active orders.
 
-	payload = {
+    payload = {
 
-		"request":"/v1/positions",
-		"nonce":genNonce()
+        "request": "/v1/orders",
+        "nonce": gen_nonce()
 
-	}
+    }
 
-	signed_payload = payloadPacker(payload)
-	r = requests.post(URL + "/positions", headers=signed_payload, verify=True)
-	rep = r.json()
+    signed_payload = payload_packer(payload)
+    r = requests.post(URL + "/orders", headers=signed_payload, verify=True)
+    rep = r.json()
 
-	return rep
+    return rep
 
-def claim_position(position_id): # Claim a position.
 
-	payload = {
+def active_positions():  # view your active positions.
 
-		"request":"/v1/position/claim",
-		"nonce":genNonce(),
-		"position_id":position_id
+    payload = {
 
-	}
+        "request": "/v1/positions",
+        "nonce": gen_nonce()
 
-	signed_payload = payloadPacker(payload)
-	r = requests.post(URL + "/position/claim", headers=signed_payload, verify=True)
-	rep = r.json()
+    }
 
-	return rep
+    signed_payload = payload_packer(payload)
+    r = requests.post(URL + "/positions", headers=signed_payload, verify=True)
+    rep = r.json()
 
-def close_position(position_id): # Claim a position.
+    return rep
 
-	payload = {
 
-		"request":"/v1/position/close",
-		"nonce":genNonce(),
-		"position_id":position_id
+def claim_position(position_id):  # Claim a position.
 
-	}
+    payload = {
 
-	signed_payload = payloadPacker(payload)
-	r = requests.post(URL + "/position/close", headers=signed_payload, verify=True)
-	rep = r.json()
+        "request": "/v1/position/claim",
+        "nonce": gen_nonce(),
+        "position_id": position_id
 
-	return rep
+    }
 
-def past_trades(timestamp=0, symbol='btcusd'): # view your past trades
+    signed_payload = payload_packer(payload)
+    r = requests.post(URL + "/position/claim", headers=signed_payload,
+                      verify=True)
+    rep = r.json()
 
-	payload = {
+    return rep
 
-		"request":"/v1/mytrades",
-		"nonce":genNonce(),
-		"symbol":symbol,
-		"timestamp":timestamp
 
-	}
+def close_position(position_id):  # Claim a position.
 
-	signed_payload = payloadPacker(payload)
-	r = requests.post(URL + "/mytrades", headers=signed_payload, verify=True)
-	rep = r.json()
+    payload = {
 
-	return rep
+        "request": "/v1/position/close",
+        "nonce": gen_nonce(),
+        "position_id": position_id
+
+    }
+
+    signed_payload = payload_packer(payload)
+    r = requests.post(URL + "/position/close", headers=signed_payload,
+                      verify=True)
+    rep = r.json()
+
+    return rep
+
+
+def past_trades(timestamp=0, symbol='btcusd'):  # view your past trades
+
+    payload = {
+
+        "request": "/v1/mytrades",
+        "nonce": gen_nonce(),
+        "symbol": symbol,
+        "timestamp": timestamp
+
+    }
+
+    signed_payload = payload_packer(payload)
+    r = requests.post(URL + "/mytrades", headers=signed_payload, verify=True)
+    rep = r.json()
+
+    return rep
+
 
 def place_offer(currency, amount, rate, period, direction):
+    payload = {
 
-	payload = {
+        "request": "/v1/offer/new",
+        "nonce": gen_nonce(),
+        "currency": currency,
+        "amount": amount,
+        "rate": rate,
+        "period": period,
+        "direction": direction
 
-		"request":"/v1/offer/new",
-		"nonce":genNonce(),
-		"currency":currency,
-		"amount":amount,
-		"rate":rate,
-		"period":period,
-		"direction":direction
+    }
 
-	}
+    signed_payload = payload_packer(payload)
+    r = requests.post(URL + "/offer/new", headers=signed_payload, verify=True)
+    rep = r.json()
 
-	signed_payload = payloadPacker(payload)
-	r = requests.post(URL + "/offer/new", headers=signed_payload, verify=True)
-	rep = r.json()
+    return rep
 
-	return rep
 
 def cancel_offer(offer_id):
+    payload = {
 
-	payload = {
+        "request": "/v1/offer/cancel",
+        "nonce": gen_nonce(),
+        "offer_id": offer_id
 
-		"request":"/v1/offer/cancel",
-		"nonce":genNonce(),
-		"offer_id":offer_id
+    }
 
-	}
+    signed_payload = payload_packer(payload)
+    r = requests.post(URL + "/offer/cancel", headers=signed_payload,
+                      verify=True)
+    rep = r.json()
 
-	signed_payload = payloadPacker(payload)
-	r = requests.post(URL + "/offer/cancel", headers=signed_payload, verify=True)
-	rep = r.json()
+    return rep
 
-	return rep
 
 def status_offer(offer_id):
+    payload = {
 
-	payload = {
+        "request": "/v1/offer/status",
+        "nonce": gen_nonce(),
+        "offer_id": offer_id
 
-		"request":"/v1/offer/status",
-		"nonce":genNonce(),
-		"offer_id":offer_id
+    }
 
-	}
+    signed_payload = payload_packer(payload)
+    r = requests.post(URL + "/offer/status", headers=signed_payload,
+                      verify=True)
+    rep = r.json()
 
-	signed_payload = payloadPacker(payload)
-	r = requests.post(URL + "/offer/status", headers=signed_payload, verify=True)
-	rep = r.json()
+    return rep
 
-	return rep
 
 def active_offers():
+    payload = {
 
-	payload = {
+        "request": "/v1/offers",
+        "nonce": gen_nonce()
 
-		"request":"/v1/offers",
-		"nonce":genNonce()
+    }
 
-	}
+    signed_payload = payload_packer(payload)
+    r = requests.post(URL + "/offers", headers=signed_payload, verify=True)
+    rep = r.json()
 
-	signed_payload = payloadPacker(payload)
-	r = requests.post(URL + "/offers", headers=signed_payload, verify=True)
-	rep = r.json()
+    return rep
 
-	return rep
 
-def balances(): # see your balances.
+def balances():  # see your balances.
 
-	payload = {
+    payload = {
 
-		"request":"/v1/balances",
-		"nonce":genNonce()
+        "request": "/v1/balances",
+        "nonce": gen_nonce()
 
-	}
+    }
 
-	signed_payload = payloadPacker(payload)
-	r = requests.post(URL + "/balances", headers=signed_payload, verify=True)
-	rep = r.json()
+    signed_payload = payload_packer(payload)
+    r = requests.post(URL + "/balances", headers=signed_payload, verify=True)
+    rep = r.json()
 
-	return rep
+    return rep
+
 
 def withdraw(withdraw_type, walletselected, amount, address):
+    payload = {
 
-	payload = {
+        "request": "/v1/withdraw",
+        "nonce": gen_nonce(),
+        "withdraw_type": withdraw_type,
+        "walletselected": walletselected,
+        "amount": amount,
+        "address": address
 
-		"request":"/v1/withdraw",
-		"nonce":genNonce(),
-		"withdraw_type":withdraw_type,
-		"walletselected":walletselected,
-		"amount":amount,
-		"address":address
+    }
 
-	}
+    signed_payload = payload_packer(payload)
+    r = requests.post(URL + "/withdraw", headers=signed_payload, verify=True)
+    rep = r.json()
 
-	signed_payload = payloadPacker(payload)
-	r = requests.post(URL + "/withdraw", headers=signed_payload, verify=True)
-	rep = r.json()
-
-	return rep
+    return rep
